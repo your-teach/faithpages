8b360f739223d5253a03253b1ecebb42005f7828
your teach <fedoriko103@gmail.com>
2024-11-17 16:11:29 +0300

    fix boolean tasks

--- main.py
+++
@@ -1,4 +0,0 @@
-print(1 > 0 and 'a' == 'b')
-print(True or -1 < 8 and (True == 0))
-print('True' != 100 and (10 != 10 or 0 == 0) and 'a' < 'b')
-print((False and True or True) and (10 > 5 and 0 > 1) or ('abc' != 'abc'))
